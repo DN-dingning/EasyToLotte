f85a5e86826bdcacc7b81f2e332d64f519611238
DN <1354772540@163.com>
2017-10-08 13:25:16 +0800

    第一次提交

--- app/src/main/java/com/dn/lotte/MainActivity.java
+++ app/src/main/java/com/dn/lotte/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends BaseActivity {
                         .subscribe(new RxSubscriber<UserInfoBean>(mContext,false) {
                             @Override
                             protected void _onNext(UserInfoBean userInfoBean) {
-
+//DDDD
                             }
 
                             @Override
